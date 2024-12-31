c8f5193a858f83d82c58910eece54f08631b3c91
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-31 13:59:28 +0300

    Create projectRoutes.js

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,8 @@
+// backend/routes/projectRoutes.js
+const express = require('express');
+const { createProject } = require('../controllers/projectController');
+
+const router = express.Router();
+router.post('/projects', createProject);
+
+module.exports = router;
