37dd7f9e2544536bec3e7b39186186227d6bc5da
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-31 13:59:59 +0300

    Create taskRoutes.js

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,8 @@
+// backend/routes/taskRoutes.js
+const express = require('express');
+const { createTask } = require('../controllers/taskController');
+
+const router = express.Router();
+router.post('/tasks', createTask);
+
+module.exports = router;
